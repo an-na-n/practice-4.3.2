e96214bc42f2a2b0266ff64999699283e7d184cb
Анна Николаева <annanikolaeva@MacBook-Air-Anna.local>
2025-09-29 01:40:48 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/practice-4.3.2/",
   test: {
     globals: true,
     environment: 'jsdom',
